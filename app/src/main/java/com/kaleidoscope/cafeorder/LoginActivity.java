06d27640f18f4c272a6eda39ddae346f8a8071a4
GregoryError <kolesnikoff2085@yandex.ru>
2022-04-13 08:07:07 +0300

    adjust order ui

--- app/src/main/java/com/kaleidoscope/cafeorder/LoginActivity.java
+++ app/src/main/java/com/kaleidoscope/cafeorder/LoginActivity.java
@@ -2,13 +2,36 @@ package com.kaleidoscope.cafeorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText edittextPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editTextName = findViewById(R.id.editTextTextName);
+        edittextPassword = findViewById(R.id.editTextPassword);
+    }
+
+    public void onClickCreateOrder(View view) {
+        String name = editTextName.getText().toString().trim();
+        String password = edittextPassword.getText().toString().trim();
+        if (!name.isEmpty() && !password.isEmpty()){
+            Intent intent = new Intent(this, CreateOrderActivity.class);
+            intent.putExtra("name", name);
+            intent.putExtra("password", password);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.toast_msg, Toast.LENGTH_SHORT).show();
+        }
     }
 }
